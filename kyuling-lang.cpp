55333aa5c8af20bc0c597bc280d3803a679d007a
Kyuling Lee <kyuling@me.com>
2020-07-01 22:43:33 +0900

    메인 함수 수정 - 실제로 동작할 수 있도록 수정
    드디어 돌려보나....;ㅅ;

--- kyuling-lang.cpp
+++ kyuling-lang.cpp
@@ -4,9 +4,23 @@
  */
 
 #include "kyuling-lang.h"
+#include "kyuling-lang-prototype.h"
 
 int main(int argc, char* argv[])
 {
-    cout << "hello world - edited" <<endl;
+    //파일이 없을 경우 - 메시지 출력 후 종료
+    if (argc == 1) 
+    { 
+        cout << "사용 방법: bbi filename\n"; 
+        exit(1); 
+    }
+
+    //내부 코드로 변환
+    convertToInternalCode(argv[1]);
+    //구분 체크
+    syntaxCheck();
+    //실행
+    execute();
+
     return 0;
 }
